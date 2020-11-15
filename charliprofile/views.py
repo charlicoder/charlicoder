3963b6e45307868f5b06e66cdaaf84f36f44c814
K Md Mamunur Rashid <charlicoder@gmail.com>
2020-11-16 04:04:32 +0600

    working on default resume page

--- charliprofile/views.py
+++ charliprofile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
-# from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View, TemplateView
 # Create your views here.
@@ -9,15 +9,17 @@ class CharliResumeView(TemplateView):
     template_name = 'charli-resume.html'
 
 
-class HomeView(View):
-
-    def get(self, request):
-        if self.request.user.is_authenticated:
-            return render(request, 'charliprofile/resume.html')
-        else:
-            return render(request, 'charli-resume.html')
+class HomeView(TemplateView):
+    template_name = 'charli-resume.html'
 
 
-# @method_decorator(login_required, name='dispatch')
+# def get(self, request):
+#     if self.request.user.is_authenticated:
+#         return render(request, 'charliprofile/resume.html')
+#     else:
+#         return render(request, 'charli-resume.html')
+
+
+@method_decorator(login_required, name='dispatch')
 class ResumeView(LoginRequiredMixin, TemplateView):
     template_name = 'resume.html'
