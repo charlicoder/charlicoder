41aa12455293365d2ef60226a585b20c88f6bc78
K Md Mamunur Rashid <charlicoder@gmail.com>
2020-11-15 23:37:34 +0600

    working with landing page

--- charliprofile/urls.py
+++ charliprofile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from charliprofile.views import HomeView, ResumeView
+from charliprofile.views import HomeView, ResumeView, CharliResumeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    # path('charli-resume/', CharliResumeView.as_view(), name="charli-resume"),
     path('resume/', ResumeView.as_view(), name="resume"),
 ]
