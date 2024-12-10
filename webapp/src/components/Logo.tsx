84e54b1c98f44d6e774c40ff6b23f6e8e2541727
Khondoker Md Mamunur Rashid <charlicoder@gmail.com>
2024-12-10 19:20:12 +0600

    added new resume

--- webapp/src/components/Logo.tsx
+++ webapp/src/components/Logo.tsx
@@ -2,17 +2,21 @@ import Typewriter from "typewriter-effect";
 
 function Logo() {
     return (
-        <Typewriter
-            options={{
-                strings: [
-                    "Charli Coder"
-                ],
-                autoStart: true,
-                loop: true,
-                deleteSpeed: 50,
-            }
-            }
-        />
+        <div style={{ fontFamily: "Dancing Script", fontSize: "24px" }}>
+
+
+            <Typewriter
+                options={{
+                    strings: [
+                        "Charli Coder"
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 50,
+                }
+                }
+            />
+        </div>
     );
 }
 
